53595c45f93834f9e588065f491fcc11f1288110
Rick Thomas <thomas.richard.l@gmail.com>
2019-12-01 21:09:30 -0500

    Updated to solve part 2

--- day1/fuel_counter_upper.py
+++ day1/fuel_counter_upper.py
@@ -1,9 +1,25 @@
-import math
+from math import floor
 
+
+# for part 1 and part 2
+def get_fuel(mass):
+    return floor(mass/3) - 2
+
+
+# for part 2 only
+def get_cumulative_fuel(mass, current_sum=0):
+    if mass <= 0:
+        return current_sum
+    current_fuel = get_fuel(mass)
+    return get_cumulative_fuel(current_fuel, current_sum + max(0, current_fuel))
+
+part_1_sum = 0
+part_2_sum = 0
 f = open('fuel_modules.txt', 'r')
 modules = [int(x.rstrip()) for x in f.readlines()]
-sum = 0
 for module in modules:
-    fuel = math.floor(module/3) - 2
-    sum += fuel
-print(sum)
+    part_1_sum += get_fuel(module)
+    part_2_sum += get_cumulative_fuel(module)
+
+print(f'Part 1: {part_1_sum}')
+print(f'Part 2: {part_2_sum}')
